0d716e22a533f058906ae36701099298768fb67e
Ian Scott <chesnekov@gmail.com>
2020-06-02 14:48:23 -0700

    a basic layout and design of a flashcard website. Not all flashcards have definitions completed, but all is functioning
    Co-authored-by: Matthew Craig <mgcraig78@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+  
+  $("h3#js").click(function() {
+    alert("An alert is a dialog box with a message")
+  })
+
+  $("h3#ops").click(function() {
+    alert("A function is a set of commands")
+  })
+
+  $("h3#var").click(function() {
+    alert("A function is a set of commands")
+  })
+
+  $("h3#names").click(function() {
+    alert("A function is a set of commands")
+  })
+
+  $("h3#function").click(function() {
+    alert("A function is a set of commands")
+  })
+
+  $("h3#methods").click(function() {
+    alert("A function is a set of commands")
+  })
+
+  $("h3#arguments").click(function() {
+    alert("A function is a set of commands")
+  })
+
+  $("h3#params").click(function() {
+    alert("A function is a set of commands")
+  })
+
+  $("h3#return").click(function() {
+    alert("A function is a set of commands")
+  })
+
+  $("h3#chaining").click(function() {
+    alert("A function is a set of commands")
+  })
+
+  $("h3#str").click(function() {
+    alert("A function is a set of commands")
+  })
+
+  $("h3#boolean").click(function() {
+    alert("A function is a set of commands")
+  })
+
+  $("h3#undefined").click(function() {
+    alert("A function is a set of commands")
+  })
+
+  $("h3#NaN").click(function() {
+    alert("A function is a set of commands")
+  })
+
+  $("h3#null").click(function() {
+    alert("A function is a set of commands")
+  })
+
+  $("h3#alert").click(function() {
+    alert("A function is a set of commands")
+  })
+
+  $("h3#comments").click(function() {
+    alert("A function is a set of commands")
+  })
+
+  $("h3#jQuery").click(function() {
+    alert("JQuery is a javascript library")
+  })
+
+  $("h3#attributes").click(function() {
+    alert("Attributes are related to CSS")
+  })
+
+});
